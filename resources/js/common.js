be9d89f4e795ae36a4c7d0ffa579e6dd0e56f6b0
ahnsungchul <81339776+ahnsungchul@users.noreply.github.com>
2025-12-11 16:51:03 +0900

    작업중

--- resources/js/common.js
+++ resources/js/common.js
@@ -1,5 +1,5 @@
 $(function() {
-
+    accoJS.init();
 });
 
 const gnb = {
@@ -23,5 +23,39 @@ const gnb = {
         setTimeout(() => {
             $(document).find(this._target).removeClass("show");
         }, 200);
+    }
+}
+
+const accoJS = {
+    _target : ".accoJS",
+    _btn : ".accoJS-btn",
+    _cont : ".accoJS-cont",
+    init(){
+        // console.log(">>> accoJS.init");
+
+        const _btn = accoJS._target+" "+accoJS._btn;
+        $(document).on("click",_btn,function(){
+            if($(this).parents(accoJS._target).hasClass("active")){
+                accoJS.hide(this);
+            }else{
+                accoJS.show(this);
+            }
+        });
+    },
+    show(_this){
+        // console.log(">>> accoJS.show");
+
+        const _target = $(_this).parents(accoJS._target);
+        const _cont = _target.find(accoJS._cont);
+        _cont.slideDown();
+        _target.addClass("active");
+    },
+    hide(_this){
+        // console.log(">>> accoJS.hide");
+
+        const _target = $(_this).parents(accoJS._target);
+        const _cont = _target.find(accoJS._cont);
+        _cont.slideUp();
+        _target.removeClass("active");
     }
 }
