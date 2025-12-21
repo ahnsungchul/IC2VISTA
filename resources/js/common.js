3a991412c17bb295bf6785989f344e401a87a285
Ahn-SungChul <93906197+Ahn-SungChul@users.noreply.github.com>
2025-12-22 00:29:00 +0900

    작업중

--- resources/js/common.js
+++ resources/js/common.js
@@ -1,6 +1,7 @@
 $(function() {
     accoJS.init();
     rangeJS.init();
+    moveTabJS.init();
 });
 
 const gnb = {
@@ -83,6 +84,30 @@ const depth3 = {
         $(document).find(depth3._target).removeClass("active");
         setTimeout(() => {
             $(document).find(depth3._target).removeClass("show");
+        }, 100);
+    }
+}
+
+const routeSnack = {
+    _target : "#main-layout .route-snack-box",
+    open(){
+        // console.log(">>> gnb.open");
+        if($(document).find(routeSnack._target).hasClass("show")){
+            return false;
+        }
+        $(document).find(routeSnack._target).addClass("show");
+        setTimeout(() => {
+            $(document).find(routeSnack._target).addClass("active");
+        }, 100);
+    },
+    close(){
+        // console.log(">>> gnb.close");
+        if(!$(document).find(routeSnack._target).hasClass("show")){
+            return false;
+        }
+        $(document).find(routeSnack._target).removeClass("active");
+        setTimeout(() => {
+            $(document).find(routeSnack._target).removeClass("show");
         }, 100);
     }
 }
@@ -189,6 +214,8 @@ const rangeJS = {
             const _rangemin = Number(_this.attr("data-rangemin"));
             const _rangemax = Number(_this.attr("data-rangemax"));
             const _rangedefault = Number(_this.attr("data-rangedefault"));
+            const _orientation = _this.attr("data-orientation") == undefined ? "horizontal" : _this.attr("data-orientation");
+            // console.log(">>> _orientation : ",_orientation);
             
             // 이미 초기화된 경우 종료
             if(_this.hasClass("js")){
@@ -203,6 +230,7 @@ const rangeJS = {
                 value: _rangedefault,
                 min: _rangemin,
                 max: _rangemax,
+                orientation:_orientation,
                 
                 // 슬라이딩 시 값 반영 (중복 로직을 updateValue로 대체)
                 slide: function( event, ui ) {
@@ -262,7 +290,82 @@ const videoSplitJS = {
     _target : '#main-video-box',
     _screenBox : '.screen-split-box',
     select(_this){
-        $(videoSplitJS._target).find(videoSplitJS._screenBox).removeClass("on");
-        $(videoSplitJS._target).find(videoSplitJS._screenBox + "." + $(_this).val()).addClass("on");
+        $(this._target).find(this._screenBox).removeClass("on");
+        $(this._target).find(this._screenBox + "." + $(_this).val()).addClass("on");
     }
 }
+
+const moveTabJS = {
+    _target : '.moveTabJS',      // 전체 컨테이너
+    _btnTab : '.btn-tab',    // 탭 개별 버튼
+    _btnCon : '.tab-con',    // 탭 콘텐츠 박스
+    _btnLeft : '.tab-left',  // 왼쪽 이동 버튼
+    _btnRight : '.tab-right',// 오른쪽 이동 버튼
+    _tabWrap : '.tab-btn', // 버튼들을 감싸고 있는 스크롤 영역
+
+    init() {
+        const self = this;
+
+        // 1. 탭 버튼 클릭 이벤트 (위임 방식)
+        $(document).on("click", `${self._target} ${self._btnTab}`, function(e) {
+            e.preventDefault();
+            const $btn = $(this);
+            const $container = $btn.closest(self._target);
+            const idx = $btn.index();
+
+            self.switchTab($container, idx);
+        });
+        
+        // 2. 왼쪽 버튼 클릭 (이전 탭으로)
+        $(document).on("click", `${self._target} ${self._btnLeft}`, function() {
+            const $container = $(this).closest(self._target);
+            self.moveTab($container, 'prev');
+        });
+
+        // 3. 오른쪽 버튼 클릭 (다음 탭으로)
+        $(document).on("click", `${self._target} ${self._btnRight}`, function() {
+            const $container = $(this).closest(self._target);
+            self.moveTab($container, 'next');
+        });
+
+        // 초기화: 첫 번째 탭 활성화
+        $(self._target).each(function() {
+            self.switchTab($(this), 0);
+        });
+    },
+
+    // 탭 전환 로직
+    switchTab($container, idx) {
+        // 버튼 활성화
+        $container.find(this._btnTab).removeClass("on").eq(idx).addClass("on");
+        
+        // 콘텐츠 활성화
+        const $targetCon = $container.find(this._btnCon);
+        $targetCon.removeClass("on").eq(idx).addClass("on");
+
+        // [연결] 탭 내부의 Swiper나 다른 JS가 있다면 재실행 (이전 slideBtnTabJS 활용)
+        if (typeof slideBtnTabJS !== 'undefined') {
+            slideBtnTabJS.init(); 
+            // 또는 특정 인스턴스 업데이트: slideBtnTabJS.reUpdate($targetCon.eq(idx).find('.slideBtnTabJS'));
+        }
+    },
+
+    // 인덱스를 계산하여 탭을 이동시키는 함수 (순환 로직 포함)
+    moveTab($container, direction) {
+        const $tabs = $container.find(this._btnTab);
+        const total = $tabs.length;
+        // 현재 활성화된('on' 클래스가 있는) 버튼의 인덱스를 찾음
+        const currentIdx = $tabs.filter('.on').index();
+        let nextIdx;
+
+        if (direction === 'prev') {
+            // 이전으로: 0에서 누르면 마지막 인덱스로 (순환)
+            nextIdx = (currentIdx - 1 + total) % total;
+        } else {
+            // 다음으로: 마지막에서 누르면 0으로 (순환)
+            nextIdx = (currentIdx + 1) % total;
+        }
+
+        this.switchTab($container, nextIdx);
+    },
+};
